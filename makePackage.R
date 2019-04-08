bd20effe6fb44771dc78f6f8da93295f6a20dfd7
danielturek <danielturek@gmail.com>
2019-04-08 13:27:11 -0400

    tided up makePackage.R

--- makePackage.R
+++ makePackage.R
@@ -1,9 +1,14 @@
 
-1
 
-library(devtools); library(roxygen2)
+## Mark:
+## use this code below to build and re-install the package,
+## when you make changes to the source files.
+
+library(devtools)
+library(roxygen2)
 baseDir <- '~/github/BayesNSGP/'
-(tarFiles <- grep('\\.tar\\.gz', list.files(baseDir, include.dirs = TRUE), value = TRUE))
+if(!('makePackage.R' %in% list.files(baseDir))) stop('change baseDir directory')
+tarFiles <- grep('\\.tar\\.gz', list.files(baseDir, include.dirs = TRUE), value = TRUE)
 for(file in tarFiles) system(paste0('rm ', file))
 system(paste0('rm -f ', paste0(baseDir, 'BayesNSGP/NAMESPACE')))
 document(paste0(baseDir, 'BayesNSGP'))
@@ -14,19 +19,28 @@ namespace <- c('import(methods)', namespace)
 namespace <- c('import(stats)',   namespace)
 writeLines(namespace, namespaceFilename)
 system(paste0('R CMD BUILD ', baseDir, 'BayesNSGP'))
-check(paste0(baseDir, 'BayesNSGP'))
-remove.packages('BayesNSGP')
+##check(paste0(baseDir, 'BayesNSGP'))
+try(remove.packages('BayesNSGP'), silent = TRUE)
 tarFiles <- grep('\\.tar\\.gz', list.files(baseDir, include.dirs = TRUE), value = TRUE)
-(lastTarFile <- tarFiles[length(tarFiles)])
+lastTarFile <- tarFiles[length(tarFiles)]
 system(paste0('R CMD install ', lastTarFile))
-qqq
-1
+
+## now quit R
+
 library(BayesNSGP)
-matern_corr
-calcQF
-calculateAD_ns
-dmnorm_nngp
+
+##
+## stop here
+##
+
+
+
+##matern_corr
+##calcQF
+##calculateAD_ns
+##dmnorm_nngp
 ##C_calcQF <- nimble::compileNimble(calcQF)
+
 
 
 library(nimble)
