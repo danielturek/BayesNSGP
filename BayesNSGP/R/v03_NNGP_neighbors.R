157ddf2f97efe08773ad46a20e0e370a3ccce61e
Fabian-Ketwaroo <139167710+Fabian-Ketwaroo@users.noreply.github.com>
2026-09-07 16:37:25 +0200

    Update v03_NNGP_neighbors.R
    indexing bug in computeNeighbors

--- BayesNSGP/R/v03_NNGP_neighbors.R
+++ BayesNSGP/R/v03_NNGP_neighbors.R
@@ -77,13 +77,15 @@ computeNeighbors <- function(coords,k , proj){
         neighbors_dist[ i, i:k] <- edist[i, i:k]
     }
     
-    for (i in (k+2):N) {
-        curr_sorted_idx = order(edist[i, ])
-        curr_sorted_idx = curr_sorted_idx[curr_sorted_idx < i] 
-        neighbor_idx[i,  ] = curr_sorted_idx[1:min(i, k)]
-        neighbors[i, neighbor_idx[i,]] = 1
-        neighbors_dist[i, 1:k ] <- edist[i, neighbor_idx[i,  ] ]
-    }
+    if ((k + 2) <= N) {
+  for (i in (k + 2):N) {
+    curr_sorted_idx <- order(edist[i, ])
+    curr_sorted_idx <- curr_sorted_idx[curr_sorted_idx < i]
+    neighbor_idx[i, ] <- curr_sorted_idx[1:min(i, k)]
+    neighbors[i, neighbor_idx[i, ]] <- 1
+    neighbors_dist[i, 1:k] <- edist[i, neighbor_idx[i, ]]
+  }
+}
     
     
     return(list(coords_sorted = coords_sorted, edist_sorted = edist, neighbors = neighbors, neighbor_idx = neighbor_idx, neighbors_dist = neighbors_dist))
